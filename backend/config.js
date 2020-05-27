1b4a5f6df060150ed91f1c4259c153854c113615
Abdul Wahab <abdulwahab@Abduls-MBP-311.fritz.box>
2020-05-27 18:21:00 +0200

    add connection URL

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/PizaShopDB",
+  Pizza_DB: process.env.MONGODB_URL || "mongodb://localhost/PizaShopDB",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
 };
